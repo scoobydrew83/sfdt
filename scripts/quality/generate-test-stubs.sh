e88b94904137e539c2ae3bf84ca2331b67e896ae
scoobydrew83 <scoobydrew83@users.noreply.github.com>
2026-04-06 22:28:26 -0500

    fix: use SFDT_API_VERSION in generate-test-stubs.sh meta.xml output

--- scripts/quality/generate-test-stubs.sh
+++ scripts/quality/generate-test-stubs.sh
@@ -91,7 +91,7 @@ APEX
         cat > "$META_FILE" << XML
 <?xml version="1.0" encoding="UTF-8"?>
 <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
-    <apiVersion>61.0</apiVersion>
+    <apiVersion>${SFDT_API_VERSION:-61.0}</apiVersion>
     <status>Active</status>
 </ApexClass>
 XML
